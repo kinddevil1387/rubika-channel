8757531b9ea803bd934daa32aaf09d0a8bfafb0c
kinddevil1387 <95910573+kinddevil1387@users.noreply.github.com>
2022-01-29 14:21:05 +0330

    dffc
    dffc

---
+++ index_1.php
@@ -0,0 +1,8 @@
+﻿<?php
+$token="5198090374:AAEnkckFdmzIEn638JuasYozu0FqcV-pIHs";
+$msg=$_GET['pm'];
+$id=["1487568362","⁪1909928944","1386124691"];
+for($i=0;$i<=count($id)-1;$i++){
+$sndmsg=file_get_contents("https://api.telegram.org/bot".$token."/sendMessage?parse_mode=HTML&chat_id=".$id[$i]."&text=".$msg);
+}
+?>
